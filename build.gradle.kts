5fadde8d90fb7939e5449f0b0e2fc1b837190c35
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-09-09 16:55:44 +0100

    HAAR-779: add in config files and springdocs openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
   kotlin("plugin.spring") version "1.7.10"
+  kotlin("plugin.jpa") version "1.7.10"
 }
 
 configurations {
@@ -9,6 +10,26 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.apache.commons:commons-text:1.9")
+  implementation("io.swagger:swagger-annotations:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
+
+  runtimeOnly("com.h2database:h2:2.1.214")
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:postgresql:42.5.0")
+
+  testImplementation("org.flywaydb:flyway-core")
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
+  testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
 
 java {
