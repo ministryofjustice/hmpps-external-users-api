9554f43ae8be7c7b442986880dcf533495fd2358
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-09-21 09:38:07 +0100

    HAAR-848: HmppsExternalUsersApiExceptionHandler optimized imports

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/HmppsExternalUsersApiExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/HmppsExternalUsersApiExceptionHandler.kt
@@ -2,10 +2,7 @@ package uk.gov.justice.digital.hmpps.externalusersapi.config
 
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.BAD_REQUEST
-import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
-import org.springframework.http.HttpStatus.CONFLICT
-import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
