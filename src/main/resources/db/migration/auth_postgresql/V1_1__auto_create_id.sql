63c9e3c740b70f036dab7b52fdc502c9b165739b
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-10-26 14:18:22 +0100

    HAAR-950 set default id creation in db

---
+++ src/main/resources/db/migration/auth_postgresql/V1_1__auto_create_id.sql
@@ -0,0 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+ALTER TABLE groups ALTER group_id SET DEFAULT gen_random_uuid();
+ALTER TABLE roles ALTER role_id SET DEFAULT gen_random_uuid();
+ALTER TABLE email_domain ALTER email_domain_id SET DEFAULT gen_random_uuid();
