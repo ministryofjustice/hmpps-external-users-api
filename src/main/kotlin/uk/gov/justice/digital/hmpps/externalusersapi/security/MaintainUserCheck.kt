4d4b1df047bb3f66d7aa4ed260805447714b641d
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-11-23 15:54:18 +0000

    HAAR-997: MaintainUserCheck re-factor to remove dependency on UserService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/security/MaintainUserCheck.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/security/MaintainUserCheck.kt
@@ -1,18 +1,19 @@
 package uk.gov.justice.digital.hmpps.externalusersapi.security
 
 import com.google.common.collect.Sets
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.toSet
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.externalusersapi.config.AuthenticationFacade
 import uk.gov.justice.digital.hmpps.externalusersapi.repository.GroupRepository
 import uk.gov.justice.digital.hmpps.externalusersapi.repository.entity.User
-import uk.gov.justice.digital.hmpps.externalusersapi.service.UserService
 
 @Service
 class MaintainUserCheck(
   private val authenticationFacade: AuthenticationFacade,
-  private val userService: UserService,
   private val groupRepository: GroupRepository
 ) {
   companion object {
@@ -25,31 +26,34 @@ class MaintainUserCheck(
   suspend fun ensureMaintainerGroupRelationship(
     maintainerName: String,
     groupCode: String,
-  ) {
-    // if they have maintain privileges then all good
+  ) = coroutineScope {
+
+    // All good if user holds maintain privilege
     if (authenticationFacade.hasRoles("ROLE_MAINTAIN_OAUTH_USERS")) {
-      return
+      return@coroutineScope
     }
-    val maintainer = userService.getUserAndGroupByUserName(maintainerName)
-    // otherwise group managers must have a group in common for maintenance
-    if (maintainer.groups.none { it.groupCode == groupCode }) {
-      // no group in common, so disallow
+
+    // Otherwise, group managers must have a group in common for maintenance
+    val groupsByUsername = async { groupRepository.findGroupsByUsername(maintainerName) }
+    if (groupsByUsername.await().toList().none { it.groupCode == groupCode }) {
+      // No group in common, so disallow
       throw GroupRelationshipException(groupCode, "Group not with your groups")
     }
   }
 
   @Throws(UserGroupRelationshipException::class)
-  suspend fun ensureUserLoggedInUserRelationship(loggedInUser: String, authorities: Collection<GrantedAuthority>, user: User) {
-    // if they have maintain privileges then all good
-    if (canMaintainUsers(authorities)) {
-      return
-    }
-    // otherwise group managers must have a group in common for maintenance
-    val loggedInUserEmail = userService.getUser(loggedInUser)
-    val userGroups = groupRepository.findGroupsByUsername(user.getUserName()).toSet()
+  suspend fun ensureUserLoggedInUserRelationship(loggedInUser: String, authorities: Collection<GrantedAuthority>, user: User) = coroutineScope {
 
-    if (Sets.intersection(loggedInUserEmail.groups, userGroups).isEmpty()) {
-      // no group in common, so disallow
+    // All good if user holds maintain privilege
+    if (canMaintainUsers(authorities)) {
+      return@coroutineScope
+    }
+    // Otherwise, group managers must have a group in common for maintenance
+    val loggedInUserGroups = async { groupRepository.findGroupsByUsername(loggedInUser) }
+    val userGroups = async { groupRepository.findGroupsByUsername(user.getUserName()) }
+
+    if (Sets.intersection(loggedInUserGroups.await().toSet(), userGroups.await().toSet()).isEmpty()) {
+      // No group in common, so disallow
       throw UserGroupRelationshipException(user.name, "User not with your groups")
     }
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/security/MaintainUserCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/security/MaintainUserCheckTest.kt
@@ -1,26 +1,24 @@
 package uk.gov.justice.digital.hmpps.externalusersapi.security
 
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThatCode
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers
 import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.externalusersapi.config.AuthenticationFacade
-import uk.gov.justice.digital.hmpps.externalusersapi.config.UserHelper.Companion.createSampleUserWithGroupAndAuthority
 import uk.gov.justice.digital.hmpps.externalusersapi.repository.GroupRepository
 import uk.gov.justice.digital.hmpps.externalusersapi.repository.entity.Group
-import uk.gov.justice.digital.hmpps.externalusersapi.service.UserService
 
 class MaintainUserCheckTest {
-  private val userService: UserService = mock()
   private val authenticationFacade: AuthenticationFacade = mock()
   private val groupRepository: GroupRepository = mock()
-  private val maintainUserCheck = MaintainUserCheck(authenticationFacade, userService, groupRepository)
+  private val maintainUserCheck = MaintainUserCheck(authenticationFacade, groupRepository)
 
   @BeforeEach
   fun initSecurityContext(): Unit = runBlocking {
@@ -30,10 +28,8 @@ class MaintainUserCheckTest {
 
   @Test
   fun `Group manager able to maintain group`(): Unit = runBlocking {
-    val groupManager =
-      createSampleUserWithGroupAndAuthority("groupManager", groups = setOf(Group("group1", "desc"), Group("group2", "desc")))
-    whenever(userService.getUserAndGroupByUserName(ArgumentMatchers.anyString()))
-      .thenReturn(groupManager)
+    whenever(groupRepository.findGroupsByUsername("groupManager")).thenReturn(flowOf(Group("group1", "desc"), Group("group2", "desc")))
+
     assertThatCode {
       runBlocking {
         maintainUserCheck.ensureMaintainerGroupRelationship(
@@ -42,15 +38,13 @@ class MaintainUserCheckTest {
         )
       }
     }.doesNotThrowAnyException()
-    verify(userService).getUserAndGroupByUserName(ArgumentMatchers.anyString())
+
+    verify(groupRepository).findGroupsByUsername(eq("groupManager"))
   }
 
   @Test
   fun `Group manager does not have group so cannot maintain`(): Unit = runBlocking {
-    val groupManager =
-      createSampleUserWithGroupAndAuthority("groupManager", groups = setOf(Group("group1", "desc"), Group("group2", "desc")))
-    whenever(userService.getUserAndGroupByUserName(ArgumentMatchers.anyString()))
-      .thenReturn(groupManager)
+    whenever(groupRepository.findGroupsByUsername("groupManager")).thenReturn(flowOf(Group("group1", "desc"), Group("group2", "desc")))
     assertThatThrownBy {
       runBlocking {
         maintainUserCheck.ensureMaintainerGroupRelationship(
@@ -60,7 +54,7 @@ class MaintainUserCheckTest {
       }
     }.isInstanceOf(GroupRelationshipException::class.java)
       .hasMessage("Unable to maintain group: group3 with reason: Group not with your groups")
-    whenever(userService.getUserAndGroupByUserName(ArgumentMatchers.anyString()))
-      .thenReturn(groupManager)
+
+    verify(groupRepository).findGroupsByUsername(eq("groupManager"))
   }
 }
