106938e41a4a472bc5ce962af3b3f2e88630b9a8
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2022-11-24 13:19:55 +0000

    HAAR-997 further GroupsService and MaintainUserCheck re-factor (#68)
    * HAAR-997: GroupsService getGroupDetail now using coroutineScope with async blocks around data retrieval
    
    * HAAR-997: Integration test correction
    
    * HAAR-997: MaintainUserCheck re-factor to remove dependency on UserService
    
    * HAAR-997: minor-format improvement
    
    * HAAR-997: code review changes
    
    * HAAR-997: further code review consistency change
    
    * HAAR-997: code review - removed unnecessary eq in test cases
    
    * HAAR-997: code review - added missing integration test case covering case where group manager attempting to view group details for group that he/she does not belong to
    
    * HAAR-997: added missing unit test cases covering ensureUserLoggedInUserRelationship on MaintainUserCheck

--- src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/UserHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/UserHelper.kt
@@ -1,8 +1,5 @@
 package uk.gov.justice.digital.hmpps.externalusersapi.config
 
-import uk.gov.justice.digital.hmpps.externalusersapi.model.User
-import uk.gov.justice.digital.hmpps.externalusersapi.repository.entity.Authority
-import uk.gov.justice.digital.hmpps.externalusersapi.repository.entity.Group
 import uk.gov.justice.digital.hmpps.externalusersapi.security.AuthSource
 import java.util.UUID
 
@@ -17,24 +14,6 @@ class UserHelper {
 
       val user = uk.gov.justice.digital.hmpps.externalusersapi.repository.entity.User(
         username = username,
-        source = source,
-      )
-      user.id = id
-      return user
-    }
-
-    fun createSampleUserWithGroupAndAuthority(
-      username: String = "firstlast",
-      authorities: Set<Authority> = emptySet(),
-      groups: Set<Group> = emptySet(),
-      source: AuthSource = AuthSource.auth,
-      id: UUID? = null,
-    ): User {
-
-      val user = User(
-        username = username,
-        authorities = authorities,
-        groups = groups,
         source = source,
       )
       user.id = id
