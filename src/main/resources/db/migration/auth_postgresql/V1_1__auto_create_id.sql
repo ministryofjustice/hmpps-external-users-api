3dd1c3fbae7f57bafe0d585b5f5efd0e38ebc100
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2023-04-06 16:53:15 +0100

    HAAR-864: updated flyway script

--- src/main/resources/db/migration/auth_postgresql/V1_1__auto_create_id.sql
+++ src/main/resources/db/migration/auth_postgresql/V1_1__auto_create_id.sql
@@ -4,3 +4,4 @@ ALTER TABLE groups ALTER group_id SET DEFAULT gen_random_uuid();
 ALTER TABLE roles ALTER role_id SET DEFAULT gen_random_uuid();
 ALTER TABLE email_domain ALTER email_domain_id SET DEFAULT gen_random_uuid();
 ALTER TABLE child_group ALTER child_group_id SET DEFAULT gen_random_uuid();
+ALTER TABLE users ALTER user_id SET DEFAULT gen_random_uuid();
--- src/main/resources/db/migration/auth_postgresql/V1_3__auto_create_id.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-ALTER TABLE users ALTER user_id SET DEFAULT gen_random_uuid();
