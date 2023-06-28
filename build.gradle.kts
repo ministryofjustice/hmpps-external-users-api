ecd8c7d6696d92d3405f3a8ae87ba35236e40d66
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-06-28 14:17:01 +0100

    HAAR-1549: ⬆️ update to spring-boot-plugin-5.2.1 (#129)
    * HAAR-1549: ⬆️ update to spring-boot-plugin-5.2.1
    
    * HAAR-1549:added google.guava to gradle file as no longer in spring boot plugin
    
    * HAAR-1549:removed configuration properties annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
-  kotlin("plugin.spring") version "1.8.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.1"
+  kotlin("plugin.spring") version "1.8.22"
 }
 
 dependencyCheck {
@@ -29,15 +29,16 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.0.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.1.Final")
 
   implementation("org.apache.commons:commons-text:1.10.0")
+  implementation("com.google.guava:guava:32.0.1-jre")
 
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.6.0")
-  implementation("io.opentelemetry:opentelemetry-api:1.26.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.27.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
 
--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/EmailDomainExclusions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/config/EmailDomainExclusions.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.externalusersapi.config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
 @Component
-@ConfigurationProperties(prefix = "application.email.domain.exclude")
 data class EmailDomainExclusions(
   @Value("\${application.email.domain.exclude}") val exclude: Set<String>,
 ) {
