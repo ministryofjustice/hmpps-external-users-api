c7289d2d26aa7506925cc8a6c28b92d8bd7bf8f4
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2023-07-28 15:49:09 +0100

    HAAR-1658: Moved method before securityCheck

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/service/UserGroupService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/service/UserGroupService.kt
@@ -44,12 +44,16 @@ class UserGroupService(
   }
 
   suspend fun getParentGroups(userId: UUID): List<GroupIdentity> {
-    checkUserGroupsReadAccess(userId)
+    if (!hasViewUserGroupsRole(authenticationFacade.getAuthentication().authorities)) {
+      userSecurityCheck(userId)
+    }
     return groupRepository.findGroupsByUserId(userId).toList()
   }
 
   suspend fun getAllGroupsUsingChildGroupsInLieuOfParentGroup(userId: UUID): List<GroupIdentity> {
-    checkUserGroupsReadAccess(userId)
+    if (!hasViewUserGroupsRole(authenticationFacade.getAuthentication().authorities)) {
+      userSecurityCheck(userId)
+    }
     val groups = groupRepository.findGroupsByUserId(userId).toList()
     val allGroups: MutableList<GroupIdentity> = mutableListOf()
 
@@ -64,12 +68,10 @@ class UserGroupService(
     return allGroups
   }
 
-  private suspend fun checkUserGroupsReadAccess(userId: UUID) {
-    if (!hasViewUserGroupsRole(authenticationFacade.getAuthentication().authorities)) {
-      userRepository.findById(userId)?.let { u: User ->
-        maintainUserCheck.ensureUserLoggedInUserRelationship(u.name)
-      } ?: throw UsernameNotFoundException("User $userId not found")
-    }
+  private suspend fun userSecurityCheck(userId: UUID) {
+    userRepository.findById(userId)?.let { u: User ->
+      maintainUserCheck.ensureUserLoggedInUserRelationship(u.name)
+    } ?: throw UsernameNotFoundException("User $userId not found")
   }
 
   @Transactional
