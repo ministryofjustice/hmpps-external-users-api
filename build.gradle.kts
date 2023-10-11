7bf3b8f88dfea27ace4f5b662d3bf0285c5fd32a
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-10-11 17:15:26 +0100

    HAAR-1945: ⬆️ update to spring-boot-plugin-5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.1"
   kotlin("plugin.spring") version "1.9.10"
 }
 
@@ -29,16 +29,16 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.5.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.6.Final")
 
   implementation("org.apache.commons:commons-text:1.10.0")
-  implementation("com.google.guava:guava:32.1.2-jre")
+  implementation("com.google.guava:guava:32.1.3-jre")
 
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.6.0")
-  implementation("io.opentelemetry:opentelemetry-api:1.30.1")
+  implementation("io.opentelemetry:opentelemetry-api:1.31.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
 
@@ -47,11 +47,11 @@ dependencies {
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.2")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.2")
   testImplementation("org.wiremock:wiremock:3.2.0")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.19.0")
+  testImplementation("org.testcontainers:postgresql:1.19.1")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
