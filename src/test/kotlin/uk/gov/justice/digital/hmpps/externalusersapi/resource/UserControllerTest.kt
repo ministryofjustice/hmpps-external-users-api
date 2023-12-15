8df7597629d143aa64387bfc126ad787bb373190
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-12-15 08:19:27 +0000

    HAAR-2154:remove unneeded mock from test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/resource/UserControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/resource/UserControllerTest.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.externalusersapi.resource
 
-import jakarta.servlet.http.HttpServletRequest
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.externalusersapi.service.CreateUserService
 import uk.gov.justice.digital.hmpps.externalusersapi.service.UserGroupService
 import uk.gov.justice.digital.hmpps.externalusersapi.service.UserSearchService
@@ -16,13 +14,11 @@ class UserControllerTest {
   private val userService: UserService = mock()
   private val userSearchService: UserSearchService = mock()
   private val userGroupService: UserGroupService = mock()
-  private val request: HttpServletRequest = mock()
   private val createUserService: CreateUserService = mock()
   private val userController = UserController(userService, userSearchService, userGroupService, createUserService)
 
   @Test
   fun enableUserByUserId(): Unit = runBlocking {
-    whenever(request.requestURL).thenReturn(StringBuffer("some/auth/url"))
     userController.enableUserByUserId(UUID.fromString("00000000-aaaa-0000-aaaa-0a0a0a0a0a0a"))
     verify(userService).enableUserByUserId(UUID.fromString("00000000-aaaa-0000-aaaa-0a0a0a0a0a0a"))
   }
