09cdb9a156306e2d49d89caf74ed0db81f5b0f09
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-12-15 12:14:30 +0000

    HAAR-2154: ⬆️ update to spring-boot-plugin-5.10.1 (#151)
    * HAAR-2154: ⬆️ update to spring-boot-plugin-5.10.1
    
    * HAAR-2154: switch to org.wiremock:wiremock-standalone:3.3.1
    
    * HAAR-2154:remove unneeded mock from test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
-  kotlin("plugin.spring") version "1.9.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.1"
+  kotlin("plugin.spring") version "1.9.21"
 }
 
 dependencyCheck {
@@ -29,29 +29,29 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.6.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.2.0.Final")
 
-  implementation("org.apache.commons:commons-text:1.10.0")
+  implementation("org.apache.commons:commons-text:1.11.0")
   implementation("com.google.guava:guava:32.1.3-jre")
 
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.6.0")
+  runtimeOnly("org.postgresql:postgresql:42.7.1")
   implementation("io.opentelemetry:opentelemetry-api")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
-  testImplementation("org.wiremock:wiremock:3.2.0")
+  testImplementation("org.wiremock:wiremock-standalone:3.3.1")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.19.1")
+  testImplementation("org.testcontainers:postgresql:1.19.3")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
