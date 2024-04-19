babeb96fac99f5b3f139f39e0c63d1979c5537f7
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-04-19 12:40:43 +0100

    HAAR-2536: ⬆️ update to spring-boot-plugin-5.15.6 (#169)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
-  kotlin("plugin.spring") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+  kotlin("plugin.spring") version "1.9.23"
 }
 
 dependencyCheck {
@@ -34,7 +34,7 @@ dependencies {
 
   implementation("org.hibernate.reactive:hibernate-reactive-core:2.2.2.Final")
 
-  implementation("org.apache.commons:commons-text:1.11.0")
+  implementation("org.apache.commons:commons-text:1.12.0")
   implementation("com.google.guava:guava:33.1.0-jre")
 
   implementation("org.flywaydb:flyway-core")
@@ -43,7 +43,7 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.7.3")
   implementation("io.opentelemetry:opentelemetry-api")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.4.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
@@ -53,7 +53,7 @@ dependencies {
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("org.wiremock:wiremock-standalone:3.4.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.19.7")
   testImplementation("io.projectreactor:reactor-test")
