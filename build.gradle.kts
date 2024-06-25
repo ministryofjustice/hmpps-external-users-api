590ada2cf1fff6e8394cd37b957f812221859ef6
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-06-25 11:17:03 +0100

    HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+  kotlin("plugin.spring") version "2.0.0"
 }
 
 dependencyCheck {
@@ -29,35 +31,37 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-audit-sdk:1.0.0")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.2.2.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.3.1.Final")
 
   implementation("org.apache.commons:commons-text:1.12.0")
-  implementation("com.google.guava:guava:33.1.0-jre")
+  implementation("com.google.guava:guava:33.2.1-jre")
 
   implementation("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
+  // runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.5.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.3")
   implementation("io.opentelemetry:opentelemetry-api")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.19.7")
+  testImplementation("org.testcontainers:postgresql:1.19.8")
   testImplementation("io.projectreactor:reactor-test")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
 
 kotlin {
@@ -65,9 +69,9 @@ kotlin {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  withType<KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "21"
+      compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
   }
 }
