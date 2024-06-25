76a1a79e4bff0eeca8cfb690a672234621f52a28
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-06-25 14:02:13 +0100

    HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc (#172)
    * HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc
    
    * HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+  kotlin("plugin.spring") version "2.0.0"
 }
-
-dependencyCheck {
-  // Suppression till can upgrade to 3.2.5
-  suppressionFiles.add("spring-suppressions.xml")
-}
-// Temporarily pin as can't upgrade to latest gradle plugin
-ext["netty.version"] = "4.1.108.Final"
 
 configurations {
   implementation { exclude(module = "spring-boot-starter-web") }
   implementation { exclude(module = "spring-boot-starter-tomcat") }
   testImplementation { exclude(group = "org.junit.vintage") }
 }
-
-// Temporarily kept at 0.9.2 as get class java.lang.Long cannot be cast to class java.lang.Integer when upgrading to 1.0.0.RELEASE
-val r2dbcPostgresVersion by extra("0.9.2.RELEASE")
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -29,35 +21,36 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-audit-sdk:1.0.0")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.2.2.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.3.1.Final")
 
   implementation("org.apache.commons:commons-text:1.12.0")
-  implementation("com.google.guava:guava:33.1.0-jre")
+  implementation("com.google.guava:guava:33.2.1-jre")
 
   implementation("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.5.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.3")
   implementation("io.opentelemetry:opentelemetry-api")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.19.7")
+  testImplementation("org.testcontainers:postgresql:1.19.8")
   testImplementation("io.projectreactor:reactor-test")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
 
 kotlin {
@@ -65,9 +58,9 @@ kotlin {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  withType<KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "21"
+      compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
   }
 }
