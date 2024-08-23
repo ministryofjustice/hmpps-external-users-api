aba3e3fa450e6b1200baa0eb2acb51480a5c247a
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-08-23 07:50:22 +0100

    HAAR-2942: actually update to spring-boot-plugin-6.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.4"
   id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
   kotlin("plugin.spring") version "2.0.20"
 }
