0008038ed6031bb703ce09c00b04c698de760ba3
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-10-24 17:17:41 +0100

    wip

--- src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/resource/OpenApiDocsTest.kt
@@ -9,6 +9,7 @@ import org.springframework.test.web.reactive.server.WebTestClient
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class OpenApiDocsTest {
