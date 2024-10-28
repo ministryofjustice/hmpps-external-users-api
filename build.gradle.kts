f298c09693ee143d75114f33524813140b48c141
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-10-28 15:23:12 +0000

    HAAR-3187 Upgraded versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
-  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
-  kotlin("plugin.spring") version "2.0.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+  kotlin("plugin.spring") version "2.0.21"
 }
 
 configurations {
@@ -24,21 +24,21 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:2.4.1.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:2.4.2.Final")
 
   implementation("org.apache.commons:commons-text:1.12.0")
-  implementation("com.google.guava:guava:33.3.0-jre")
+  implementation("com.google.guava:guava:33.3.1-jre")
 
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.5.RELEASE")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.4")
   implementation("io.opentelemetry:opentelemetry-api")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -46,9 +46,9 @@ dependencies {
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.9.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.9.2")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.20.1")
+  testImplementation("org.testcontainers:postgresql:1.20.3")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
