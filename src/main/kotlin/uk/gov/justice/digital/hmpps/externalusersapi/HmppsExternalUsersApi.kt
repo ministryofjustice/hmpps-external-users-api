c1c5d33ccb2f948f30c6ee32f9475618266ecbf6
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-10-30 16:07:46 +0000

    fix?

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/HmppsExternalUsersApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalusersapi/HmppsExternalUsersApi.kt
@@ -2,8 +2,10 @@ package uk.gov.justice.digital.hmpps.externalusersapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication()
+@ComponentScan("uk.gov.justice.hmpps.sqs.audit")
 class HmppsExternalUsersApi
 
 fun main(args: Array<String>) {
