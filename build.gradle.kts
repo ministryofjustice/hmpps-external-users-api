b2ef16972a2836d06829be0bff3d1df0c8888644
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-01-29 16:12:04 +0000

    HAAR-3577: 🔧 upgrade to hmpps.gradle-spring-boot 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.0.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.0.0"
   id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
   kotlin("plugin.spring") version "2.1.10"
 }
