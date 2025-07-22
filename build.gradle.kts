348ae7dcc0e89f3b7037cc663118f9ef03f49498
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-07-22 11:05:06 +0100

    HAAR-4144: ⬆️ update to spring-boot-plugin-8.3.3 (#196)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
-  id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
-  kotlin("plugin.spring") version "2.1.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.3"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
+  kotlin("plugin.spring") version "2.2.0"
 }
 
 configurations {
@@ -19,12 +19,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.9")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:3.0.1.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:3.0.5.Final")
 
   implementation("org.apache.commons:commons-text:1.13.1")
   implementation("com.google.guava:guava:33.4.8-jre")
@@ -38,8 +38,8 @@ dependencies {
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.9")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
@@ -48,7 +48,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.21.2")
+  testImplementation("org.testcontainers:postgresql:1.21.3")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
