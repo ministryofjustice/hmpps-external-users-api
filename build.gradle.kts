82cd693f21059e7dee7c1b9ed8fbd27594d237e2
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-08-28 20:49:47 +0100

    HAAR-4257: ⬆️ update to spring-boot-plugin-9.0.0 (#200)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
   id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
   kotlin("plugin.spring") version "2.2.10"
 }
