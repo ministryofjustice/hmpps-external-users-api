0e1bb89fd8cb25fc4a51eb98718c7f906e55b6b4
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-11-25 15:20:09 +0000

    HAAR-4693: ⬆️ update to spring-boot-plugin-9.2.0 (#210)
    * HAAR-4693: ⬆️ update to spring-boot-plugin-9.2.0
    
    * HAAR-4693: keep springdoc-openapi-starter-webflux-ui at 2.8.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.2.0"
   id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
   kotlin("plugin.spring") version "2.2.21"
 }
@@ -19,26 +19,26 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.6.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.6.2")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
 
-  implementation("org.hibernate.reactive:hibernate-reactive-core:4.1.3.Final")
+  implementation("org.hibernate.reactive:hibernate-reactive-core:4.1.7.Final")
 
   implementation("org.apache.commons:commons-text:1.14.0")
   implementation("com.google.guava:guava:33.5.0-jre")
 
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  runtimeOnly("org.postgresql:r2dbc-postgresql:1.1.0.RELEASE")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:1.1.1.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.8")
   implementation("io.opentelemetry:opentelemetry-api")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.13")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -46,7 +46,7 @@ dependencies {
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.13.0")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.13.0")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.21.3")
   testImplementation("io.projectreactor:reactor-test")
