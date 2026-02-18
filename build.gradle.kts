bdbd91bcc9603db0c771e30973a805b4edee24bb
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-02-18 08:49:27 +0000

    HAAR-4916: remove un-needed spring-boot-starter-webclient as we have webflux

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,13 @@ configurations {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-flyway")
-  implementation("org.springframework.boot:spring-boot-starter-webclient")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-  implementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-flyway")
+  implementation("org.springframework.boot:spring-boot-starter-gson")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
-  implementation("org.springframework.boot:spring-boot-starter-gson")
+  implementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.security:spring-security-access")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.0.0")
 
@@ -32,29 +31,25 @@ dependencies {
 
   implementation("org.apache.commons:commons-text:1.15.0")
   implementation("com.google.guava:guava:33.5.0-jre")
-
   implementation("org.flywaydb:flyway-core")
+  implementation("io.opentelemetry:opentelemetry-api")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.1")
+
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:r2dbc-postgresql:1.1.1.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.10")
-  implementation("io.opentelemetry:opentelemetry-api")
-
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.1")
-
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
-  testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.springframework.boot:spring-boot-starter-data-r2dbc-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-gson-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-client-test")
   testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server-test")
   testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-  testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-client-test")
-  testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
-  testImplementation("org.springframework.boot:spring-boot-starter-gson-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
