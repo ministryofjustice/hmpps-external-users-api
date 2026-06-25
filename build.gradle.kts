cc2f5a01f299cd5761a988e6a224a3edebd524f8
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-06-25 14:10:28 +0100

    HAAR-5647: ⬆️ update to spring-boot-plugin-10.5.3
    also Sign images etc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.3"
   id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0"
   kotlin("plugin.spring") version "2.4.0"
 }
